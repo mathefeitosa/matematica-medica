0f0449271af017d9026b57a894751d68820eb317
Feitosa <mathe.feitosa@gmail.com>
2025-04-17 17:24:20 -0300

    erro, ajuste chato
    Novo teste

---
+++ src/views/DrogasNovo.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-expansion-panels v-model="panels" multiple>
+        <v-col cols="12" sm="12" md="6" lg="4">
+          <Metricas></Metricas>
+        </v-col>
+        <v-col cols="12" xm="12" md="6" lg="4">
+          <CalculadoraConcentracao></CalculadoraConcentracao>
+        </v-col>
+        <v-col cols="12" xm="12" md="6" lg="4">
+          <DoseDrogas></DoseDrogas>
+        </v-col>
+      </v-expansion-panels>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Metricas from "../components/Metricas.vue";
+import CalculadoraConcentracao from "../components/CalculadoraConcentracao.vue";
+import CalculadoraNova from "../components/CalculadoraNova.vue";
+
+export default {
+  components: { Metricas, CalculadoraConcentracao, CalculadoraNova },
+  name: "Drogas",
+  data() {
+    return {
+      panels: [0],
+    };
+  },
+};
+</script>
